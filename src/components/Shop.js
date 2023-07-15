7a8759da2b6f541bc604d51dc3e7ded75ead9c18
Zak-2005 <zak.mob2005@gmail.com>
2023-07-15 13:17:52 -0500

    Render Products based off filters

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,71 @@
-const Shop = () =>{
-    return <div>Shop</div>
-}
+import { Outlet } from "react-router-dom";
+import Card from "./Card";
+import Filter from "./Filter";
+import { products } from "./ProductList";
+import { useEffect, useState } from "react";
+const Shop = () => {
+  const [utensilFilter, setUtensilFilter] = useState(false);
+  const [dishwareFilter, setDishwareFilter] = useState(false);
+  const [napkinFilter, setNapkinFilter] = useState(false);
+  useEffect(()=>
+    {
+        console.log(utensilFilter, dishwareFilter, napkinFilter)
+    });
+  return (
+    <div className="shopContent">
+      <div className="filter">
+      <Filter
+          label="Utensils"
+          setChecked={setUtensilFilter}
+        />
+        <Filter
+          label="Dishware"
+          setChecked={setDishwareFilter}
+        />
+        <Filter
+          label="Napkins"
+          setChecked={setNapkinFilter}
+        />
+      </div>
+      <div className="products">
+        {products.map((product) => {
+          if (!utensilFilter && !dishwareFilter && !napkinFilter) {
+            return (
+              <Card
+                src={`./Images/${product.title}.png`}
+                title={product.title}
+                price={product.price}
+              />
+            );
+          } else if (utensilFilter && product.type === "Utensil") {
+            return (
+              <Card
+                src={`./Images/${product.title}.png`}
+                title={product.title}
+                price={product.price}
+              />
+            );
+          } else if (dishwareFilter && product.type === "Dishware") {
+            return (
+              <Card
+                src={`./Images/${product.title}.png`}
+                title={product.title}
+                price={product.price}
+              />
+            );
+          } else if (napkinFilter && product.type === "Napkin") {
+            return (
+              <Card
+                src={`./Images/${product.title}.png`}
+                title={product.title}
+                price={product.price}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Shop;
