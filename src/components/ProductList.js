6200f8436c406df48e42b265a4f9c67778bc3d3e
Zak-2005 <zak.mob2005@gmail.com>
2023-07-15 13:19:30 -0500

    List of products to be rendered

---
+++ src/components/ProductList.js
@@ -0,0 +1,40 @@
+export const products = [
+  { title: "Exquisite-Spork", price: "$125", type: "Utensil" },
+  {
+    title: "Exquisite-Splayd",
+    price: "$155",
+    type: "Utensil"
+  },
+  {
+    title: "Wooden-Spoon",
+    price: "$15",
+    type: "Utensil"
+  },
+  { title: "Blate", price: "$165",
+  type: "Dishware" },
+  {
+    title: "Bowl",
+    price: "$54",
+    type: "Dishware"
+  },
+  {
+    title: "Plate",
+    price: "$54",
+    type: "Dishware"
+  },
+  {
+    title: "White-Napkins",
+    price: "$20",
+    type: "Napkin"
+  },
+  {
+    title: "Blue-Napkins",
+    price: "$20",
+    type: "Napkin"
+  },
+  {
+    title: "Green-Napkins",
+    price: "$20",
+    type: "Napkin"
+  },
+];
