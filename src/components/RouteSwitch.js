fba88cfc3f386c6d74265888f66067f022a129ff
Zak-2005 <zak.mob2005@gmail.com>
2023-07-15 13:19:40 -0500

    Paths for all pages

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,17 +1,33 @@
-import { HashRouter,Route,Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, useOutletContext } from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop";
 import NavBar from "./NavBar";
-const RouteSwitch = () =>{
-    return (
-        <HashRouter>
-            <Routes>
-                <Route element={<NavBar />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                </Route>
-            </Routes>
-        </HashRouter>
-    )
-}
-export default RouteSwitch
+import "../styles.css";
+import CardPage from "./CardPage";
+import { products } from "./ProductList";
+import React, {useState} from "react";
+import Cart from "./Cart";
+const RouteSwitch = () => {
+    const [itemsInCart, setItemsInCart] = useState(0);
+  return (
+    <HashRouter>
+      <Routes>
+        <Route element={<NavBar itemsInCart={itemsInCart}/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart itemsInCart={itemsInCart}/>} />
+          <Route path="/shop">
+            <Route path="products" element={<Shop />} />
+
+            {products.map((product) => {
+              return <Route
+                path={product.title}
+                element={<CardPage title={product.title} price={product.price} itemsInCart={itemsInCart} setItemsInCart={setItemsInCart}/>}
+              />;
+            })}
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
+export default RouteSwitch;
